16528b989e8c24ae1bb3eba79acd96c2479c2b4f
Zh3RM <shuzhufilms@gmail.com>
2022-07-28 23:30:25 -0700

    initial commit

---
+++ asset/script.js
@@ -0,0 +1,27 @@
+descriptionEl = document.querySelectorAll(".description");
+
+titleEl = document.querySelectorAll(".title");
+
+
+for (i = 0; i < descriptionEl.length; i++) {
+    descriptionEl[i].setAttribute("data-id", i)
+    titleEl[i].setAttribute("data-id", i)
+
+}
+
+for (i = 0; i < titleEl.length; i++) {
+    titleEl[i].addEventListener("click", function (event) {
+        var getId = event.target.getAttribute("data-id")
+        var showTitle = titleEl[getId]
+        var showDescription = descriptionEl[getId]
+
+        if (showDescription.style.display === "none") {
+            showDescription.setAttribute("style", "display: block");
+            showTitle.setAttribute("style", "animation: pulse 0.1s infinite")
+        } else {
+            showDescription.setAttribute("style", "display:none")
+            showTitle.setAttribute("style", "animation: none")
+        }
+
+    })
+}
