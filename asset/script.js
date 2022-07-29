47ea5fbabcc78cad23b335a21a31439bee6878fc
Zh3RM <shuzhufilms@gmail.com>
2022-07-29 01:17:20 -0700

    revised screening info

--- asset/script.js
+++ asset/script.js
@@ -4,24 +4,26 @@ titleEl = document.querySelectorAll(".title");
 
 
 for (i = 0; i < descriptionEl.length; i++) {
-    descriptionEl[i].setAttribute("data-id", i)
-    titleEl[i].setAttribute("data-id", i)
+    descriptionEl[i].setAttribute("data-id", i);
+    titleEl[i].setAttribute("data-id", i);
+}
+
+var showHide = function (event) {
+    var getId = event.target.getAttribute("data-id");
+    var showTitle = titleEl[getId];
+    var showDescription = descriptionEl[getId];
+
+    if (showDescription.style.display === "none") {
+        showDescription.setAttribute("style", "display: block");
+        showTitle.setAttribute("style", "animation: pulse 0.1s infinite");
+    } else {
+        showDescription.setAttribute("style", "display:none");
+        showTitle.setAttribute("style", "animation: none");
+    }
 
 }
 
 for (i = 0; i < titleEl.length; i++) {
-    titleEl[i].addEventListener("click", function (event) {
-        var getId = event.target.getAttribute("data-id")
-        var showTitle = titleEl[getId]
-        var showDescription = descriptionEl[getId]
-
-        if (showDescription.style.display === "none") {
-            showDescription.setAttribute("style", "display: block");
-            showTitle.setAttribute("style", "animation: pulse 0.1s infinite")
-        } else {
-            showDescription.setAttribute("style", "display:none")
-            showTitle.setAttribute("style", "animation: none")
-        }
-
-    })
+    titleEl[i].addEventListener("click", showHide)
 }
+
